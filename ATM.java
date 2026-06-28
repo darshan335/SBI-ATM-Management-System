3d067fb86488f5ed282d014ab892f0870f0f078e
darshan335 <darshan4106@gmail.com>
2026-06-28 19:55:59 +0530

    Added ATM.java file

---
+++ ATM.java
@@ -0,0 +1,32 @@
+package com.project;
+
+class ATM {
+	private double depositAmount;
+	private double withdrawAmount;
+	private double balanceAmount;
+
+	public double getDepositAmount() {
+		return depositAmount;
+	}
+
+	public void setDepositAmount(double depositAmount) {
+		this.depositAmount = depositAmount;
+	}
+
+	public double getWithdrawAmount() {
+		return withdrawAmount;
+	}
+
+	public void setWithdrawAmount(double withdrawAmount) {
+		this.withdrawAmount = withdrawAmount;
+	}
+
+	public double getBalanceAmount() {
+		return balanceAmount;
+	}
+
+	public void setBalanceAmount(double balanceAmount) {
+		this.balanceAmount = balanceAmount;
+	}
+
+}
